d6dccc222f41dd257ceef5d462d77f8d6b7786ad
monkeychen <cza55008@gmail.com>
2026-07-19 17:09:32 +0800

    wx-kit 0.5.5

--- Casks/wx-kit.rb
+++ Casks/wx-kit.rb
@@ -2,11 +2,11 @@ cask "wx-kit" do
   version "0.5.5"
 
   on_arm do
-    sha256 "bb3c023c612c3c01cd56be21cc3e94a0e49ccf68221aa0487e076e258724fc6d"
+    sha256 "92b144abd4514ec2e8813509d5b557601e60d08215bd969d2f07ec578aecace9"
     url "https://github.com/monkeychen/wx-kit/releases/download/v#{version}/wx-kit-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "e4bd48dc2fba84f20da3415b6b70140b84bb533d502300be2e5b7525440341d6"
+    sha256 "31ec28b65687b7cbc710a52f70f873cb91d263366d9cacf09bbbc1c1f030417a"
     url "https://github.com/monkeychen/wx-kit/releases/download/v#{version}/wx-kit-#{version}.dmg"
   end
 
